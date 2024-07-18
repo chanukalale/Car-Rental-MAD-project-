47080b757940ef68a7b5760e37eab22b164bebf1
Chanuka Lalanaka <152151845+chanukalale@users.noreply.github.com>
2024-07-18 14:31:31 +0530

    customer and employee home nagigations

--- app/src/main/java/com/example/testingmadapp/EmployeeHome/EmployeeActivity.java
+++ app/src/main/java/com/example/testingmadapp/EmployeeHome/EmployeeActivity.java
@@ -1,26 +1,80 @@
 package com.example.testingmadapp.EmployeeHome;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.testingmadapp.EmployeeHome.EmpFragments.AddCarsFragment;
+import com.example.testingmadapp.EmployeeHome.EmpFragments.AllcarsFragment;
+import com.example.testingmadapp.EmployeeHome.EmpFragments.YourCarsFragment;
 import com.example.testingmadapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EmployeeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bnav;
+    private FrameLayout frm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_employee);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bnav = findViewById(R.id.bnav);
+        frm = findViewById(R.id.frm);
+
+        loadFragment(new YourCarsFragment(), false);
+
+        bnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.yourCars){
+                    loadFragment(new YourCarsFragment(), false);
+
+                }else if(itemId == R.id.addCars){
+                    loadFragment(new AddCarsFragment(), false);
+
+                }
+//                else if(itemId == R.id.signout){
+//                    loadFragment(new AddCarsFragment(), false);
+//
+//                }
+                else if(itemId == R.id.allCars){
+                    loadFragment(new AllcarsFragment(), false);
+
+                }
+
+                return true;
+            }
         });
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInsialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(isAppInsialized){
+            fragmentTransaction.add(R.id.frm, fragment);
+
+        }else{
+
+            fragmentTransaction.replace(R.id.frm, fragment);
+        }
+
+        fragmentTransaction.commit();
     }
 }
